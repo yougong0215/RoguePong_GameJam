caef0cbc395a1bdf68f9b8c3a7fadea497590fbd
Adobe <adobeserverpackbackup2@gmail.com>
2024-07-13 02:06:54 +0900

    Apply HitModule

--- Assets/01_Script/Map/BounceGlobe.cs
+++ Assets/01_Script/Map/BounceGlobe.cs
@@ -2,33 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BounceGlobe : MonoBehaviour
+public class BounceGlobe : MonoBehaviour, HitModule
 {
-    private BoxColliderCast boxcast;
     // Start is called before the first frame update
     void Start()
     {
-        boxcast = GetComponent<BoxColliderCast>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (boxcast.ReturnColliders().Length > 0)
-        {
-            TriggerEnter(boxcast.ReturnColliders()[0]);
-        }
+
     }
 
-    private void TriggerEnter(Collider other)
+    public void HitBall(BallSystem bss)
     {
-        if (other.TryGetComponent<BallSystem>(out BallSystem bss))
-        {
-            bss._abilityStat._addSpeed = Mathf.Clamp(bss._abilityStat._addSpeed + 1, 0, 120);
-            bss.SetttingDir(transform.forward);
-            Debug.Log(transform.forward);
-            //bss.WallCollisionItem(GetComponent<Collider>());
-            bss.RefreshTime();
-        }
+        bss._abilityStat._addSpeed = Mathf.Clamp(bss._abilityStat._addSpeed + 1, 0, 120);
+        bss.SetttingDir(transform.forward);
+        Debug.Log(transform.forward);
+        //bss.WallCollisionItem(GetComponent<Collider>());
+        bss.RefreshTime();
     }
 }
