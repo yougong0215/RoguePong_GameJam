0bc259af194abdc54221593d6f8cf2be89b132ae
yuchul06 <65890837+yuchul06@users.noreply.github.com>
2024-07-13 06:28:28 +0900

    카드 ui 만들어보기

---
+++ Assets/01_Script/UI/CardUI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CardUI : MonoBehaviour
+{
+    public Ability ablity;
+
+    public Image IconImg;
+    public TextMeshProUGUI NameTxt;
+    public TextMeshProUGUI DescTxt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
