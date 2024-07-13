05302d4ee37522196bf6e01c84a75bb7c922c764
sungmin0427 <yoonsm0037@gmail.com>
2024-07-14 03:34:03 +0900

    볼들 다 추가했읍
    ㅇㅇ

--- Assets/01_Script/ScriptableObject/BallData.cs
+++ Assets/01_Script/ScriptableObject/BallData.cs
@@ -1,11 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 
-[Serializable]
-public class BallAsset
+[CreateAssetMenu(menuName ="SO/BallInfo")]
+public class BallAsset : Ability
 {
     public BallEnum BallType;
     public GameObject BallMesh;
@@ -14,6 +15,14 @@ public class BallAsset
     public float MultySize = 1.0f;
     public float MultyATK = 1.0f;
 
+    public override void GetAbility(ref ObjectSystem ballStat)
+    {
+        ballStat._originStat = new();
+        ballStat._originStat._multySpeed = MultySpeed;
+        ballStat._originStat._multySize = MultySize;
+        ballStat._originStat._multyATK = MultyATK;
+        GameObject.FindObjectsOfType<BallSystem>().ToList().ForEach((b) => { b.SettingBallType(BallType); });
+    }
 }
 
 [CreateAssetMenu(fileName = "BallData")]
